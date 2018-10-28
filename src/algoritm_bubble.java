6e56a3ef92fa3d409d3ad8029569b37c9151f9e9
Ann-Yuryeva <anutca57@mail.ru>
2018-10-28 22:41:19 +0300

    Задача №14: Сортировка данных в массиве

---
+++ src/algoritm_bubble.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class algoritm_bubble {
+    public static void main(String[] args) {
+        /*Необходимо написать программу для сортировки данных массива по возрастанию.
+Использовать пузырьковый метод сортировки.*/
+        //Создание массива
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите длину массива: ");
+        int mas_long = in.nextInt();
+        System.out.println("длина массива: " + mas_long);
+        int[] massiv = new int[mas_long];
+        System.out.println("Заполните числами массив: ");
+        for (int i = 0; i < mas_long; i++) {
+            massiv[i] = in.nextInt();
+        }System.out.println("Массив =");
+        System.out.println(Arrays.toString(massiv));
+        // сортировка заданного массива
+        System.out.println("Сортировка массива ");
+         for (int k = massiv.length - 1; k > 0; k--) {
+            for (int l = 0; l < k; l++) {
+             if (massiv[l] > massiv[l + 1]) {
+               int tmp = massiv[l];
+               massiv[l] = massiv[l + 1];
+               massiv[l + 1] = tmp;
+                        }
+                    }
+                }System.out.println("Резульат сортировки =");
+        System.out.println(Arrays.toString(massiv));
+            }
+        }
